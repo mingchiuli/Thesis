9de24eb1185d306904d3c8b858b85c71b4b1df5d
mingchiuli <mingchiuli@163.com>
2023-01-27 00:54:07 +0800

    20230127

--- Thesis.R
+++ Thesis.R
@@ -16,7 +16,7 @@ lapply(c('tidyverse', 'stargazer', 'plm', 'sandwich', 'lmtest', 'ggpubr', 'showt
 ```"
  
 title <- '---
-title: "戒急用忍：台湾地区的南向经济"
+title: "戒急用忍：台湾地区的南向政策"
 author:
   - 李鸣玖
 documentclass: ctexart
@@ -36,7 +36,7 @@ rmd <- list.files(pattern = '*.Rmd', recursive = T)
 chunk <- str_c(title, "\n", init, "\n")
 chunks <- str_c("```{r child = '", rmd, "'}\n```" ,"\n", "\\newpage")
 
-chunks <- c(chunk, chunks, "# 参考文献\n")
+chunks <- c(chunk, chunks, "\n# 参考文献\n")
 
 writeLines(chunks, "out.rmd")
 
