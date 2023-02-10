df83c43bf7c2c26dbbf85ba5fd7a97fbac1b6ce3
mingchiuli <mingchiuli@163.com>
2023-02-11 02:47:03 +0800

    20230211

--- Thesis.R
+++ Thesis.R
@@ -5,17 +5,28 @@ if (system.file(package = "stringr") == '') {
 library(stringr)
 
 init <- "```{r, include=FALSE}
-lapply(c('tidyverse', 'stargazer', 'plm', 'sandwich', 'lmtest', 'ggpubr', 'showtext', 'rticles', 'maps', 'see'), function(pkg) {
-   if (system.file(package = pkg) == '') {
-     install.packages(pkg)
-   }
-   library(pkg, character.only = TRUE)
-})
+library(purrr)
+c('tidyverse', 
+  'stargazer', 
+  'plm', 
+  'sandwich', 
+  'lmtest', 
+  'ggpubr', 
+  'showtext', 
+  'rticles', 
+  'maps', 
+  'see') |> 
+  purrr::map(\\(pkg) {
+    if (system.file(package = pkg) == '') {
+      install.packages(pkg)
+    }
+    library(pkg, character.only = TRUE)
+  })
 showtext_auto()
 twData <- read_csv('data/data.csv')
 ```"
  
-title <- '---
+header <- '---
 title: "戒急用忍：台湾地区的南向政策"
 author:
   - 李鸣玖
@@ -37,12 +48,10 @@ bibliography: bibliography.bib
 ---'
  
 rmd <- list.files(pattern = '*.Rmd', recursive = T)
-chunk <- str_c(title, "\n", init, "\n")
+chunk <- str_c(header, "\n", init, "\n")
 chunks <- str_c("```{r child = '", rmd, "'}\n```" ,"\n", "\\newpage")
 chunks <- c(chunk, chunks, "\n# 参考文献\n")
 writeLines(chunks, "out.rmd")
 rmarkdown::render("out.rmd")
 file.remove('out.rmd', 'out.log')
 system2("open",'out.pdf')
-
-
