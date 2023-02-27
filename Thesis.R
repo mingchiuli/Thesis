5eb52f9fb2580e881aab115e64d5e171f559e3b8
mingchiuli <mingchiuli@163.com>
2023-02-28 00:35:42 +0800

    20230228

--- Thesis.R
+++ Thesis.R
@@ -16,7 +16,7 @@ output:
     latex_engine: xelatex
     fig_caption: yes
     number_sections: yes
-    toc_depth: 2
+    toc_depth: 3
     toc: yes
 bibliography: bibliography.bib
 ---"
@@ -76,26 +76,15 @@ fn_state <- c('China', 'India', 'Pakistan', 'Bangladesh', 'Nepal', 'Sri Lanka', 
 fn_first_state <- c('Thailand','Malaysia', 'Indonesia', 'Philippines', 'Singapore', 'Vietnam', 'Brunei')
 fn_second_state <- c('Laos', 'Myanmar', 'Cambodia', 'Australia', 'New Zealand')
 
-build_d <- Vectorize(\\(state, year) {
-  if (state %in% fn_first_state) {
-    return(year - 1994)
-  } else if (state %in% fn_second_state) {
-    return(year - 1997)
-  } else {
-    return(NA)
-  }
-})
-
 fn_data <- tw_data |> 
   filter(Year < 2003, Year >= 1990, State %in% fn_state) |> 
   rename(政策干预 = Treat, 出口占比 = ExpPerc, 进口占比 = InpPerc, 投资占比 = FDIPerc, 外交关系 = Diplomatic, 外交持续 = duDiplomatic, 世贸组织 = WTO_Y_IN_TW_IN, 亚太经合 = APEC, 开放度 = Openness, 人口 = LPop, 生产总值 = LGDP) |> 
-  mutate(fn_d = build_d(State, Year))
+  mutate(fn_d = if_else(State %in% fn_first_state, Year - 1994, if_else(State %in% fn_second_state, Year - 1997, NA)))
   
 NSBP_data <- tw_data |> 
   filter(Year >= 2008) |> 
   rename(政策干预 = Treat, 出口占比 = ExpPerc, 进口占比 = InpPerc, 投资占比 = FDIPerc, 外交关系 = Diplomatic, 外交持续 = duDiplomatic, 世贸组织 = WTO_Y_IN_TW_IN, 两岸协议 = ECFA, 开放度 = Openness, 人口 = LPop, 生产总值 = LGDP, 自贸协定 = FTA) |> 
   mutate(nsbp_d = if_else(State %in% NSBP, Year - 2016, NA))  
-
 ```"
 
 list.files(pattern = '*.Rmd', recursive = T) |> 
